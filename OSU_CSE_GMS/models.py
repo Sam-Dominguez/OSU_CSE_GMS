ea8714cfa4d256fcd3e936182569a7c3d38464ce
Sam-Dominguez <samdominguez@gmail.com>
2024-01-31 16:44:03 -0500

    Create database models and migrations

---
+++ OSU_CSE_GMS/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    # Using IntegerField instead of BooleanField b/c SQLLite does not support boolean values: (1 = True, 0 = False)
+    in_columbus = models.IntegerField()
+    previous_grader = models.IntegerField()
+
+    previous_classes = models.TextField(blank=True)
+
+class Administrator(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Instructor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Course(models.Model):
+    course_number = models.CharField(primary_key=True, max_length=15)
+    name = models.CharField(max_length=30)
+
+class Section(models.Model):
+    course_number = models.ForeignKey("Course", on_delete=models.CASCADE)
+    section_number = models.CharField(max_length=30)
+    instructor = models.ForeignKey("Instructor", on_delete=models.SET_DEFAULT, default='No Instructor')
+    
+    INSTRUCTION_MODES = [
+        ('SYNCHRNONOUS', 'SYNCHRNONOUS'),
+        ('ASYNCHRNONOUS', 'ASYNCHRNONOUS')
+    ]
+    instruction_mode = models.CharField(max_length=13, choices=INSTRUCTION_MODES)
+    
+    # term (AU, SU, SP) + year (XXXX)
+    semester = models.CharField(max_length=6)
+
+    time = models.CharField(max_length=30, blank=True)
+    days_of_week = models.CharField(max_length=30, blank=True)
+    classroom = models.CharField(max_length=30, blank=True)
+
+    class Meta:
+        unique_together = [['course_number', 'section_number']]
+
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# How to Run
-`python3 manage.py runserver`
+# Set UP
+
+## Create Virtual Envrionment
+Windows
+`py -m venv OSU_CSE_GMS`
+
+Mac
+`python -m venv OSU_CSE_GMS`
+
+
+## Activate VENV
+Windows
+`OSU_CSE_GMS\Scripts\activate.bat`
+
+Mac
+`source OSU_CSE_GMS/bin/activate`
+
+## Clone the Project
+`git clone https://github.com/Sam-Dominguez/OSU-CSE-GMS.git`
+
+## Install django
+Windows
+`py -m pip install Django`
+
+Mac
+`python -m pip install Django`
+
+## Run the Migrations
+`python manage.py migrate OSU_CSE_GMS`
+
+# Post Set Up
+Each time you revist the project:
+1. Activate VENV
+2. Pull latest from repository
+3. Run the migrations
+4. Run the server: `python manage.py runserver`
+
+# Database Commands
+
+## Create Migrations
+`python manage.py makemigrations OSU_CSE_GMS`
+
+## Run the Migrations
+`python manage.py migrate OSU_CSE_GMS`
