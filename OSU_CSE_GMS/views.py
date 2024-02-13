4b90c852bcc8e7d287c85ecb18bf844320996da7
Matthew McLain <mwm.mclain@gmail.com>
2024-02-12 22:16:39 -0500

    Added table for courses and sorting functionality

--- OSU_CSE_GMS/views.py
+++ OSU_CSE_GMS/views.py
@@ -13,11 +13,27 @@ def administrator(request):
 
     # Fetch all existing courses from the database
     courses = Course.objects.all()
+    sort_direction = 'asc'
+
+    # If the 'sort' parameter is in the request, toggle the sort direction
+    if 'sort' in request.GET:
+        sort_direction = request.GET['sort']
+        sort_direction = 'asc' if sort_direction == 'desc' else 'desc'
+
+    # Sort courses based on the sort direction
+    if sort_direction == 'asc':
+        courses = courses.order_by('course_number')
+        sort_text = 'Sorting (asc)'
+    else:
+        courses = courses.order_by('-course_number')
+        sort_text = 'Sorting (desc)'
 
     # Pass the form and courses to the template context
     context = {
         'form': form,
         'courses': courses,
+        'sort_direction': sort_direction,
+        'sort_text': sort_text
     }
     
     return render(request, 'administrator.html', context)
